8528e2f9f52cb4a7f9540c7077dfedb5de597cf5
Dagestan05 <steklofan@gmail.com>
2019-01-01 16:29:02 +0100

    added mongoose connection, bodyParser MW

--- server/server.js
+++ server/server.js
@@ -1,5 +1,21 @@
+require("dotenv").config();
 const express = require('express');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+
 const app = express();
- 
+
+// mongoDB setup
+mongoose.Promise = global.Promise;
+const dbURI = process.env.MONGO_URI;
+mongoose.connect(dbURI, { useNewUrlParser: true }, ()=>console.log("connected"))
+
+//BodyParser settings
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+// server
 const port = process.env.PORT || 3000;
 app.listen(port, ()=>console.log(`Server running on port ${port}`));
