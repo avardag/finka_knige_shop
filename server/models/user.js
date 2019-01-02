74d047955b52eaf24d91c9a1a3f3271ed6e1b00c
Dagestan05 <steklofan@gmail.com>
2019-01-02 07:57:13 +0100

    added preSave passowrd hashing MW to user model

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const SALT_I = 10;
 
 const userSchema = mongoose.Schema({
   email:{
@@ -39,6 +41,23 @@ const userSchema = mongoose.Schema({
   }
 })
 
+//Middleware to hash password before save
+userSchema.pre("save", function(next){
+  let user = this; //not required in arrow func
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+  // generate a salt
+  bcrypt.genSalt(SALT_I, function(err, salt){
+    if(err) return next(err)
+    // hash the password along with our new salt
+    bcrypt.hash(user.password, salt, function(err, hash){
+      if(err) return next(err);
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    })
+  })
+})
 
 const User = mongoose.model("User", userSchema);
 
