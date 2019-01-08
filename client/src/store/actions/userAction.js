eacff8865fc146305469de5b95a172c5e29c1512
Dagestan05 <steklofan@gmail.com>
2019-01-08 07:18:22 +0100

    created action types file, api endoints constats file, and started useractions file

---
+++ client/src/store/actions/userAction.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+import { USER_ROUTES } from '../../utils/misc';
+import { LOGIN } from './types';
+
+
---
+++ client/src/utils/misc.js
@@ -0,0 +1,6 @@
+//server api endpoints
+export const USER_ROUTES = "/api/users"
+//for adding styles or brands
+export const PRODUCT_ROUTES = "/api/product"
+//for getting products by queries r all products
+export const PRODUCTS_ROUTES = "/api/products"
