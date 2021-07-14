b8c126628a9a93d798512d4a343492442932b507
avardag <steklofan@gmail.com>
2021-07-14 20:17:37 +0200

    fixed product imaes lightbox, changed libs

--- client/src/components/Product/ProductImages.js
+++ client/src/components/Product/ProductImages.js
@@ -1,85 +1,69 @@
-import React, { Component } from 'react';
-import ImageLightbox from '../utils/ImageLightbox';
+import React, { useEffect, useState } from "react";
+import FsLightbox from "fslightbox-react";
 
-class ProductImages extends Component {
-  state = {
-    lightboxIsOpen: false,
-    imageIndex: 0,
-    lightboxImages: []
-  }
-  componentDidMount() {
+export default function ProductImages({ prodDetail }) {
+  //component state
+  const [toggler, setToggler] = useState(false);
+  const [imageIndex, setImageIndex] = useState(0);
+  const [lightboxImages, setLightboxImages] = useState([]);
+
+  //useEffect, build images array from props
+  useEffect(() => {
     //if there are images of a product
-    if (this.props.prodDetail.images.length > 0) {
-      let lightboxImages = this.props.prodDetail.images
-      this.setState({ lightboxImages })
+    if (prodDetail.images.length > 0) {
+      let lightboxImagesArr = [];
+      for (let img of prodDetail.images) {
+        lightboxImagesArr.push(img.url);
+      }
+      setLightboxImages(lightboxImagesArr);
     }
-  }
+  }, []);
   //gets url of images for div bacckground. Or Not_Found_Pic if no images exist
-  renderCardImage = (images) => {
+  const renderCardImage = (images) => {
     if (images.length > 0) {
-      return images[0].url
+      return images[0].url;
     } else {
-      return '/images/image_not_available.png'
+      return "/images/image_not_available.png";
     }
-  }
+  };
 
-  handleLightboxClick = (imageIndex) => {
-    if (this.state.lightboxImages.length > 0) {
-      this.setState({
-        lightboxIsOpen: true,
-        imageIndex
-      })
+  // displays lightbox when clicked on img
+  const handleLightboxClick = (imageIndex) => {
+    if (lightboxImages.length > 0) {
+      setImageIndex(imageIndex);
+      setToggler(!toggler);
     }
-  }
-  handleLightboxClose = () => {
-    this.setState({
-      lightboxIsOpen: false
-    })
-  }
-
+  };
   //renders prod images under main pic, starting from 2nd img(if there are)
-  showThumbs = (prodDetail) => (
-    this.state.lightboxImages.map((item, i) => (
-      i > 0 ?
+  const renderThumbs = (prodDetail) =>
+    lightboxImages.map((item, i) =>
+      i > 0 ? (
         <div
           key={i}
-          onClick={() => this.handleLightboxClick(i)}
+          onClick={() => handleLightboxClick(i)}
           className="thumb"
-          style={{ background: `url(${item.url}) no-repeat` }}
+          style={{ background: `url(${item}) no-repeat` }}
         ></div>
-        : null
-    ))
-  )
+      ) : null
+    );
 
   //RENDER
-  render() {
-    const { prodDetail } = this.props;
-
-    return (
-      <div className="product_image_container">
-        <div className="main_pic">
-          <div
-            style={{ background: `url(${this.renderCardImage(prodDetail.images)}) no-repeat` }}
-            onClick={() => this.handleLightboxClick(0)}
-          >
-          </div>
-          <div className="main_thumbs">
-            {this.showThumbs(prodDetail)}
-          </div>
-        </div>
-        {
-          this.state.lightboxIsOpen &&
-          <ImageLightbox
-            id={prodDetail._id}
-            images={this.state.lightboxImages}
-            open={this.state.lightboxIsOpen}
-            imageIndex={this.state.imageIndex}
-            onClose={() => this.handleLightboxClose()}
-          />
-        }
+  return (
+    <div className="product_image_container">
+      <div className="main_pic">
+        <div
+          style={{
+            background: `url(${renderCardImage(prodDetail.images)}) no-repeat`,
+          }}
+          onClick={() => handleLightboxClick(0)}
+        ></div>
+        <div className="main_thumbs">{renderThumbs(prodDetail)}</div>
       </div>
-    );
-  }
+      <FsLightbox
+        toggler={toggler}
+        sources={lightboxImages}
+        sourceIndex={imageIndex}
+      />
+    </div>
+  );
 }
-
-export default ProductImages;
