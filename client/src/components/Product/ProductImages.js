1e006b5a4bc2097ff3d52bef881a356072171a8d
avardag <steklofan@gmail.com>
2021-07-17 16:31:21 +0200

    small fix for lightbox not showing img

--- client/src/components/Product/ProductImages.js
+++ client/src/components/Product/ProductImages.js
@@ -64,6 +64,7 @@ export default function ProductImages({ prodDetail }) {
         toggler={toggler}
         sources={lightboxImages}
         sourceIndex={imageIndex}
+        type="image"
       />
     </div>
   );
