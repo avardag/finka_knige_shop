8313197b361181e9ed68d6e6f635af6c57b8ff49
avardag <steklofan@gmail.com>
2023-01-22 16:30:30 +0100

    try serve static 1

--- server/server.js
+++ server/server.js
@@ -55,6 +55,7 @@ app.use("/api/site", require("./routes/siteInfo"));
 //   });
 // }
 if (process.env.NODE_ENV === "production") {
+  console.log("production statuc");
   app.use(express.static(path.join(__dirname, "../client/build")));
   app.use((req, res) => {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
