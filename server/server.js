93ca786b32f4407117bdffa40a3d6c3fa2bd2250
avardag <steklofan@gmail.com>
2023-01-22 18:41:20 +0100

    try serve static 10

--- server/server.js
+++ server/server.js
@@ -80,5 +80,5 @@ const PORT = process.env.PORT || 5000;
 
 //Connect to the database before listening
 connectDB().then(() => {
-  app.listen(PORT, () => console.log(`Server running on port : ${port}`));
+  app.listen(PORT, () => console.log(`Server running on port : ${PORT}`));
 });
